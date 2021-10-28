6ddade659d36070626db3849762ffbecf2d32c1b
SHAILESH <shaileshdubeyr@gmail.com>
2021-10-28 05:44:37 +0530

    Added : code for happy and sad message

--- UserValidatorTest/src/test/java/com/uservalidation/UserValidatorTest.java
+++ UserValidatorTest/src/test/java/com/uservalidation/UserValidatorTest.java
@@ -7,9 +7,37 @@ public class UserValidatorTest {
    private Assertions Assert;
 
    @Test
-   public void givenPasswordShouldReturnTrueWhenSpecialCharaterhaveAnyOne(){
+   public void givenFirstNameWhenPropershouldReturnTrue() {
       UserValidator userDetails = new UserValidator();
-         boolean testStatus = userDetails.gmailTest("abc+100@gamil.com");
-         Assert.assertTrue(testStatus);
+      boolean testStatus = userDetails.firstName("Shailesh");
+      Assert.assertTrue(testStatus);
+   }
+
+   @Test
+   public void givenLastNameWhenPropershouldReturnTrue() {
+      UserValidator userDetails = new UserValidator();
+      boolean testStatus = userDetails.lastName("Dubey");
+      Assert.assertTrue(testStatus);
+   }
+
+   @Test
+   public void givenPasswordShouldReturnTrueWhenSpecialCharaterhaveAnyOne() {
+      UserValidator userDetails = new UserValidator();
+      boolean testStatus = userDetails.passwordTest("Shailesh@889");
+      Assert.assertTrue(testStatus);
+   }
+
+   @Test
+   public void givenEmailShouldReturnTrueWhenalltheConditionSatisfied() {
+      UserValidator userDetails = new UserValidator();
+      boolean testStatus = userDetails.gmailTest("Shailesh@jvj123");
+      Assert.assertTrue(testStatus);
+   }
+
+   @Test
+   public void givenMessageShouldReturnSadWhenTestIsPassed(){
+      MoodAnalyser mood =  new MoodAnalyser();
+      String testStatus = mood.messageContainHappyReturnHappyElseSad("This is Sad message");
+      Assert.assertEquals("Sad",testStatus);
    }
 }
