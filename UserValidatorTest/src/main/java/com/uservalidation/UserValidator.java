fc5495f6e7ba8a6b3cdb3a65c81fb8a2551dd248
SHAILESH <shaileshdubeyr@gmail.com>
2021-10-28 23:26:30 +0530

    Added : code for parameterised test using email

--- UserValidatorTest/src/main/java/com/uservalidation/UserValidator.java
+++ UserValidatorTest/src/main/java/com/uservalidation/UserValidator.java
@@ -1,13 +1,15 @@
 package com.uservalidation;
 
 import java.util.regex.Pattern;
-import java.util.regex.Matcher;
 
 public class UserValidator {
-    private static final String FIRST_NAME_PATTERN = "^[A-Z]{1}[a-zA-Z]{3,15}$";
+    private static final String EMAIL_PATTERN = "^[a-zA-Z0-9+_.-]+[@]{1}[a-zA-Z0-9]+[.a-z]{3,4}([.a-zA-Z]{3})*$";
 
-    public boolean firstName(String firstName) {
-        boolean mathPattern = Pattern.matches(FIRST_NAME_PATTERN, firstName);
-        return mathPattern;
+    public static boolean gmailTest(String email, boolean actualValue) {
+        boolean matchesString = Pattern.matches(EMAIL_PATTERN, email);
+        if(matchesString == actualValue)
+            return actualValue;
+        else
+            return false;
     }
 }
--- UserValidatorTest/src/test/java/com/uservalidation/UserValidatorTest.java
+++ UserValidatorTest/src/test/java/com/uservalidation/UserValidatorTest.java
@@ -1,15 +1,57 @@
 package com.uservalidation;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.util.Arrays;
+
+@RunWith(value = Parameterized.class)
 public class UserValidatorTest {
-   private Assertions Assert;
+   private  UserValidator validator;
+   private static String emailPattern;
+   private static boolean expectedResult;
+
+   public UserValidatorTest(String emailPattern, boolean expectedResult){
+      this.emailPattern = emailPattern;
+      this.expectedResult = expectedResult;
+   }
+
+   @Before
+   public void insilized(){
+      validator = new UserValidator();
+   }
+   @Parameterized.Parameters(name= "{index}: isValid({0})={1}")
+   public static Iterable<Object[]> data() {
+      return Arrays.asList(new Object[][]{
+                      {"abc-100@yahoo.com", true},
+                      {"abc.100@yahoo.com", true},
+                      {"abc111@gmail.com", true},
+                      {"abc-100@abc.com", true},
+                      {"abc-100@abc.com", true},
+                      {"abc@gmail.com.com", true},
+                      {"abc@gmail.com.com", true},
+                      {"abc@1.com", true},
+                      {"abc+100@gmail.com", true},
+                      {"abc", false},
+                      {".abc+100@gmail.com", false},
+                      {"abc+100@gmail.com", false},
+                      {"abc+100@gmail.a", false},
+                      {"abc+100@.gmail.com", false},
+                      {"abc+100*()@gmail.com", false}
+              }
+      );
+   }
 
    @Test
    public void givenFirstNameWhenPropershouldReturnTrue() {
-      UserValidator userDetails = new UserValidator();
+      /*UserValidator userDetails = new UserValgidator();
       boolean testStatus = userDetails.firstName("Shailesh");
-      Assert.assertTrue(testStatus);
+      Assert.assertTrue(testStatus);*/
+     boolean actualParameter = UserValidator.gmailTest(emailPattern, expectedResult);
+      System.out.println("sum of numbers = : "+actualParameter);
+      Assert.assertEquals(expectedResult,actualParameter);
    }
 }
