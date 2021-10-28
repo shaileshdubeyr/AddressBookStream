c09d24092cd947e64de490d0b1730edc7a6efac5
SHAILESH <shaileshdubeyr@gmail.com>
2021-10-28 05:59:12 +0530

    Aedded code for uc 1

--- UserValidatorTest/src/test/java/com/uservalidation/UserValidatorTest.java
+++ UserValidatorTest/src/test/java/com/uservalidation/UserValidatorTest.java
@@ -12,32 +12,4 @@ public class UserValidatorTest {
       boolean testStatus = userDetails.firstName("Shailesh");
       Assert.assertTrue(testStatus);
    }
-
-   @Test
-   public void givenLastNameWhenPropershouldReturnTrue() {
-      UserValidator userDetails = new UserValidator();
-      boolean testStatus = userDetails.lastName("Dubey");
-      Assert.assertTrue(testStatus);
-   }
-
-   @Test
-   public void givenPasswordShouldReturnTrueWhenSpecialCharaterhaveAnyOne() {
-      UserValidator userDetails = new UserValidator();
-      boolean testStatus = userDetails.passwordTest("Shailesh@889");
-      Assert.assertTrue(testStatus);
-   }
-
-   @Test
-   public void givenEmailShouldReturnTrueWhenalltheConditionSatisfied() {
-      UserValidator userDetails = new UserValidator();
-      boolean testStatus = userDetails.gmailTest("Shailesh@jvj123");
-      Assert.assertTrue(testStatus);
-   }
-
-   @Test
-   public void givenMessageShouldReturnSadWhenTestIsPassed(){
-      MoodAnalyser mood =  new MoodAnalyser();
-      String testStatus = mood.messageContainHappyReturnHappyElseSad("This is Sad message");
-      Assert.assertEquals("Sad",testStatus);
-   }
 }
