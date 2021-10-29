e0d0181085cfb6b0343aa1bb8dfeaa3c4be5bfc9
SHAILESH <shaileshdubeyr@gmail.com>
2021-10-29 07:53:26 +0530

    Added : Code for Exception handleing

--- UserValidatorTest/src/main/java/com/uservalidation/UserValidator.java
+++ UserValidatorTest/src/main/java/com/uservalidation/UserValidator.java
@@ -1,15 +1,81 @@
 package com.uservalidation;
 
+import java.util.InputMismatchException;
 import java.util.regex.Pattern;
 
 public class UserValidator {
-    private static final String EMAIL_PATTERN = "^[a-zA-Z0-9+_.-]+[@]{1}[a-zA-Z0-9]+[.a-z]{3,4}([.a-zA-Z]{3})*$";
+    private static final String EMAIL_PATTERN = "^[a-zA-Z0-9+_.-]+[@]{1}[a-zA-Z0-9]+[.a-z]{4}([.a-zA-Z]{3})*$";
+    private static final String FIRST_NAME_PATTERN = "^[A-Z]{1}[a-zA-Z]{3,15}$";
+    private static final String LAST_NAME_PATTERN = "^[A-Z]{1}[a-zA-Z]{3,15}$";
+    private static final String MOBILE_NO_PATTERN = "^[0-9]{2}[ ][0-9]{10}$";
+    private static final String PASSWORD_PATTERN = "([!@#$%^&*()]?[A-Za-z0-9]+){8,}$";
 
-    public static boolean gmailTest(String email, boolean actualValue) {
-        boolean matchesString = Pattern.matches(EMAIL_PATTERN, email);
-        if(matchesString == actualValue)
-            return actualValue;
+    public boolean firstName(String firstName) {
+        try {
+            boolean mathPattern = Pattern.matches(FIRST_NAME_PATTERN, firstName);
+            if(mathPattern == true)
+                return mathPattern;
+            else
+                throw (new InputMismatchException("given Input is not correct"));
+        }
+        catch (InputMismatchException inputMismatch){
+            System.out.println("Exception "+inputMismatch.getMessage());
+            return  false;
+        }
+    }
+
+    public boolean lastName(String lastName) {
+        try {
+            boolean mathPattern = Pattern.matches(LAST_NAME_PATTERN, lastName);
+            if(mathPattern == true)
+                return mathPattern;
+            else
+                throw (new InputMismatchException("given Input is not correct"));
+        }
+        catch (InputMismatchException inputMismatch){
+            System.out.println("Exception "+inputMismatch.getMessage());
+            return  false;
+        }
+    }
+
+    public boolean mobileNumber(String mobileNumber) {
+        try {
+            boolean mathPattern = Pattern.matches(MOBILE_NO_PATTERN, mobileNumber);
+        if(mathPattern == true)
+            return mathPattern;
         else
-            return false;
+            throw (new InputMismatchException("given Input is not correct"));
+    }
+    catch (InputMismatchException inputMismatch){
+        System.out.println("Exception "+inputMismatch.getMessage());
+        return  false;
+        }
+    }
+
+    public boolean passwordTest(String password) {
+        try {
+            boolean mathPattern = Pattern.matches(PASSWORD_PATTERN, password);
+            if(mathPattern == true)
+                return mathPattern;
+            else
+                throw (new InputMismatchException("given Input is not correct"));
+        }
+        catch (InputMismatchException inputMismatch){
+            System.out.println("Exception "+inputMismatch.getMessage());
+            return  false;
+        }
+    }
+    public boolean gmailTest(String email) {
+        try {
+            boolean mathPattern = Pattern.matches(EMAIL_PATTERN, email);
+            if(mathPattern == true)
+                return mathPattern;
+            else
+                throw (new InputMismatchException("given Input is not correct"));
+        }
+        catch (InputMismatchException inputMismatch){
+            System.out.println("Exception "+inputMismatch.getMessage());
+            return  false;
+        }
     }
 }
--- UserValidatorTest/src/test/java/com/uservalidation/UserValidatorTest.java
+++ UserValidatorTest/src/test/java/com/uservalidation/UserValidatorTest.java
@@ -1,54 +1,42 @@
 package com.uservalidation;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 
-import java.util.Arrays;
+public class UserValidatorTest{
 
-@RunWith(value = Parameterized.class)
-public class UserValidatorTest {
-   private  UserValidator validator;
-   private static String emailPattern;
-   private static boolean expectedResult;
-
-   public UserValidatorTest(String emailPattern, boolean expectedResult){
-      this.emailPattern = emailPattern;
-      this.expectedResult = expectedResult;
-   }
-
-   @Before
-   public void insilized(){
-      validator = new UserValidator();
-   }
-   @Parameterized.Parameters(name= "{index}: isValid({0})={1}")
-   public static Iterable<Object[]> data() {
-      return Arrays.asList(new Object[][]{
-                      {"abc-100@yahoo.com", true},
-                      {"abc.100@yahoo.com", true},
-                      {"abc111@gmail.com", true},
-                      {"abc-100@abc.com", true},
-                      {"abc-100@abc.com", true},
-                      {"abc@gmail.com.com", true},
-                      {"abc@gmail.com.com", true},
-                      {"abc@1.com", true},
-                      {"abc+100@gmail.com", true},
-                      {"abc", false},
-                      {".abc+100@gmail.com", false},
-                      {"abc+100@gmail.com", false},
-                      {"abc+100@gmail.a", false},
-                      {"abc+100@.gmail.com", false},
-                      {"abc+100*()@gmail.com", false}
-              }
-      );
+   @Test
+   public void givenFirstNameWhenPropershouldReturnTrue() {
+      UserValidator userDetails = new UserValidator();
+      boolean testStatus = userDetails.firstName("Shailesh");
+      Assert.assertTrue(testStatus);
    }
 
    @Test
-   public void givenInputFOrEmailWhenPropershouldOrNotReturnTrue() {
-     boolean actualParameter = UserValidator.gmailTest(emailPattern, expectedResult);
-      System.out.println("sum of numbers = : "+actualParameter);
-      Assert.assertEquals(expectedResult,actualParameter);
+   public void givenLastNameWhenPropershouldReturnTrue() {
+      UserValidator userDetails = new UserValidator();
+      boolean testStatus = userDetails.lastName("Dubey");
+      Assert.assertTrue(testStatus);
+   }
+
+   @Test
+   public void givenPasswordShouldReturnTrueWhenSpecialCharaterhaveAnyOne() {
+      UserValidator userDetails = new UserValidator();
+      boolean testStatus = userDetails.passwordTest("Shailesh@889");
+      Assert.assertTrue(testStatus);
+   }
+
+   @Test
+   public void givenInputMobilenumberReturnTrueWhenSpaceAfterSpace(){
+      UserValidator userDetails = new UserValidator();
+      boolean testStatus = userDetails.mobileNumber("91 7046909396");
+      Assert.assertTrue(testStatus);
+   }
+
+   @Test
+   public void givenEmailShouldReturnTrueWhenalltheConditionSatisfied() {
+      UserValidator userDetails = new UserValidator();
+      boolean testStatus = userDetails.gmailTest("Shailesh@jvj123");
+      Assert.assertTrue(testStatus);
    }
 }
