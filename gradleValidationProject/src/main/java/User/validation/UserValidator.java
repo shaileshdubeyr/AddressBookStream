f0d583fb9957c5e083f4347d3dafc0db439fd56a
shaileshdubeyr <54022188+shaileshdubeyr@users.noreply.github.com>
2022-01-21 12:21:42 +0530

    Delete gradleValidationProject directory

--- gradleValidationProject/src/main/java/User/validation/UserValidator.java
+++
@@ -1,9 +0,0 @@
-package User.validation;
-import java.util.regex.Pattern;
-
-public class UserValidator {
-    public boolean returnFirstNameStatus() {
-        boolean mathcPattern = Pattern.matches("^[A-Z]{1}[a-zA-Z]{3,15}$", "Shailesh");
-        return mathcPattern;
-    }
-}
